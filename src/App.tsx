696554ac3e4276f9a8c91c194e655cf7311d4abf
safwan-ms <safwan.anasms@gmail.com>
2025-07-21 19:37:30 +0530

    chore: Made Setup by installing dependencies for Notes Project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
+};
+export default App;
